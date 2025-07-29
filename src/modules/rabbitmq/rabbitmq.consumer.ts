1b0e8797b5ab9b44904571852ea3b8ae7fcbe929
GuticaStefan <stefan.gutica@gmail.com>
2025-07-29 17:54:57 +0300

    remove system contract address

--- src/modules/rabbitmq/rabbitmq.consumer.ts
+++ src/modules/rabbitmq/rabbitmq.consumer.ts
@@ -42,7 +42,7 @@ export class RabbitMqConsumer {
                     if(rawEventType.identifier === 'delegateVote') {
                         rawEventType.topics =  [rawEventType.topics[1], rawEventType.topics[2], rawEventType.topics[0], rawEventType.topics[3], rawEventType.topics[4]] 
                     } else if (rawEventType.identifier === 'vote') {
-                        rawEventType.address = governanceConfig.onChain.linear[0];
+                        // rawEventType.address = governanceConfig.onChain.linear[0]; // TODO: check
                         const hexAddress = Buffer.from(rawEventType.address, 'utf-8').toString('hex');
                         const base64Address = Buffer.from(hexAddress, 'hex').toString('base64');
                         rawEventType.topics = [rawEventType.topics[1], base64Address, rawEventType.topics[0], rawEventType.topics[2], rawEventType.topics[3]];
